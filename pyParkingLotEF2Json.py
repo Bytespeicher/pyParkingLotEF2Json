2c052a539e321b2d6061575f10e6c3cb397a0771
MKzero <info@linux-web-development.de>
2015-10-12 09:05:17 +0200

    PEP8ify

--- pyParkingLotEF2Json.py
+++ pyParkingLotEF2Json.py
@@ -7,44 +7,55 @@ import re
 import datetime
 
 static_data = []
-static_data.append({ 'latitude':"50.9725714", 'longitude':"11.0338341" , 'adresse':"Thomasstraße, 99084 Erfurt"})
-static_data.append({ 'latitude':"50.9843184", 'longitude':"11.0311626" , 'adresse':"Wallstraße, 99084 Erfurt"})
-static_data.append({ 'latitude':"50.97828", 'longitude':"11.02073" , 'adresse':"Bechtheimer Straße 1, 99084 Erfurt"})
-static_data.append({ 'latitude':"50.973088", 'longitude':"11.037674" , 'adresse':"Willy-Brandt-Platz 2, 99084 Erfurt"})
-static_data.append({ 'latitude':"50.9779121", 'longitude':"11.0370504" , 'adresse':"Fleischgasse 2, 99084 Erfurt"})
-static_data.append({ 'latitude':"50.9734289", 'longitude':"11.0330801" , 'adresse':"Forum 1, Lachsgasse"})
-static_data.append({ 'latitude':"50.9744646", 'longitude':"11.0330455" , 'adresse':"Hirschlachufer 7, 99084 Erfurt"})
-static_data.append({ 'latitude':"50.970866", 'longitude':"11.018662" , 'adresse':"Bonifaciusstraße 15, 99084 Erfurt"})
+static_data.append({'latitude': "50.9725714", 'longitude': "11.0338341",
+                    'adresse': "Thomasstraße, 99084 Erfurt"})
+static_data.append({'latitude': "50.9843184", 'longitude': "11.0311626",
+                    'adresse': "Wallstraße, 99084 Erfurt"})
+static_data.append({'latitude': "50.97828", 'longitude': "11.02073",
+                    'adresse': "Bechtheimer Straße 1, 99084 Erfurt"})
+static_data.append({'latitude': "50.973088", 'longitude': "11.037674",
+                    'adresse': "Willy-Brandt-Platz 2, 99084 Erfurt"})
+static_data.append({'latitude': "50.9779121", 'longitude': "11.0370504",
+                    'adresse': "Fleischgasse 2, 99084 Erfurt"})
+static_data.append({'latitude': "50.9734289", 'longitude': "11.0330801",
+                    'adresse': "Forum 1, Lachsgasse"})
+static_data.append({'latitude': "50.9744646", 'longitude': "11.0330455",
+                    'adresse': "Hirschlachufer 7, 99084 Erfurt"})
+static_data.append({'latitude': "50.970866", 'longitude': "11.018662",
+                    'adresse': "Bonifaciusstraße 15, 99084 Erfurt"})
+
 
 class ParkingLotEF2Json():
     page = ""
     data = []
-    
+
     def __init__(self):
         url = "http://stadtplan.erfurt.de/cgi-bin/grafik.plx?L=de&PS=24&T=600&MAP=aspf&Mode=0&ME=3&X=15&Y=15&OBJ="
         user_agent = 'Mozilla/5.0 (Windows NT 6.3; WOW64; rv:36.0) Gecko/20100101 Firefox/36.0'
-        headers = { 'User-Agent' : user_agent }
+        headers = {'User-Agent': user_agent}
         req = urllib2.Request(url, None, headers)
         response = urllib2.urlopen(req)
         self.page = response.read()
-        
+
     def getJson(self):
         m = re.findall(r'Ob(.*von.*);', self.page)
         i = 0
         time = str(datetime.datetime.now())
-        self.data.append({'date' : time})
+        self.data.append({'date': time})
+
         for elem in m:
-            name = re.search(r'"[ A-Za-z0-9äöü+-]+"', elem, re.UNICODE).group(0)[1:-1]
+            name = re.search(r'"[ A-Za-z0-9äöü+-]+"', elem,
+                             re.UNICODE).group(0)[1:-1]
             belegt = re.search(r'[\-0-9]+\)', elem).group(0)[0:-1]
             max = re.search(r'von.*,', elem).group(0)[5:-1]
-            tmp = { 'name':name, 'belegt':belegt, 'maximal':max }
+            tmp = {'name': name, 'belegt': belegt, 'maximal': max}
             tmp.update(static_data[i])
             self.data.append(tmp)
-            i=i+1
-            
+            i = i + 1
+
         return json.dumps(self.data, ensure_ascii=False)
-    
-out = ParkingLotEF2Json().getJson();
+
+out = ParkingLotEF2Json().getJson()
 
 fo = open("data.json", "wb")
 fo.write(out)
